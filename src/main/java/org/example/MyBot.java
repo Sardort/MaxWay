392635380c13c21e8e7d7fbb692a6ba65a49ca5c
Sardor <sardort0424@gmail.com>
2024-10-23 18:57:36 +0500

    gfgfgfgfgfgfgfg

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -1,25 +1,71 @@
 package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.List;
 
 public class MyBot extends TelegramLongPollingBot {
 
+    private MyBotService myBotService = new MyBotService();
 
     @Override
     public String getBotUsername() {
-        return "";
+        return "MaxwayCloneBot";
     }
 
     @Override
     public void onUpdateReceived(Update update) {
 
+        if (update.hasMessage() && update.getMessage().hasText()){
+            String text = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
+            String firstName = update.getMessage().getChat().getFirstName();
+            String lastName = update.getMessage().getChat().getLastName();
+
+            info(chatId,text,firstName,lastName);
+
+            if (text.equals("/start")){
+                try {
+                    execute(myBotService.language(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (text.equals("O'zbek tili")){
+                try {
+                    execute(myBotService.mainMenuUzbek(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+
+            /*if (text.equals("/start")){
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setChatId(chatId);
+                sendMessage.setText("Salom");
+
+                try {
+                    execute(sendMessage);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }*/
+        }
+
     }
 
     @Override
     public String getBotToken() {
-        return super.getBotToken();
+        return "7573595803:AAHfkYMaT-K7haa613rHdG5t2nVbX_gRLrE";
+    }
+
+    public void info (Long chatId, String text, String firstname, String lastname){
+        System.out.println(" ChatId " + chatId + "\n" + " Text " + text + "\n" +
+                " Firsname " + firstname + "\n" + " Lastname " + lastname);
     }
 }
--- src/main/java/org/example/MyBotService.java
+++ src/main/java/org/example/MyBotService.java
@@ -1,4 +1,72 @@
 package org.example;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyBotService {
+
+    public SendMessage sendMessage(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Salom");
+        return sendMessage;
+    }
+    public SendMessage language(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Xush kelibsiz / Добро пожаловать" + "\n" + "Tilni tanlang / Выберите язык");
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> rowList = new ArrayList<>();
+
+        KeyboardRow row = new KeyboardRow();
+        KeyboardButton button = new KeyboardButton();
+        button.setText("O'zbek tili");
+        row.add(button);
+
+        KeyboardButton button1 = new KeyboardButton();
+        button1.setText("Русский язык");
+        row.add(button1);
+
+        rowList.add(row);
+
+        replyKeyboardMarkup.setKeyboard(rowList);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        return sendMessage;
+    }
+
+    public SendMessage mainMenuUzbek(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> rowList = new ArrayList<>();
+
+        KeyboardRow row = new KeyboardRow();
+        KeyboardButton button = new KeyboardButton();
+        button.setText("Buyurtma berish");
+        row.add(button);
+        rowList.add(row);
+
+        KeyboardRow row1 = new KeyboardRow();
+        KeyboardButton button1 = new KeyboardButton();
+        button1.setText("Aksiya");
+        row1.add(button1);
+
+        KeyboardButton button2 = new KeyboardButton();
+        button2.setText("Barcha filiallar");
+        row1.add(button2);
+        rowList.add(row1);
+
+        replyKeyboardMarkup.setKeyboard(rowList);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        return sendMessage;
+    }
 }
