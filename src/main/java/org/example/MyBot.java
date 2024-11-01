7225eb8b3a79a4d3c94b2af4e5b2ab1db670b67c
Sardor <sardort0424@gmail.com>
2024-11-01 11:24:54 +0500

    add main4

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -47,14 +49,6 @@ public class MyBot extends TelegramLongPollingBot {
                 }
             }
 
-            if (text.equals("🛍" + "Buyurtma berish")){
-                try {
-                    execute(myBotService.orderMenu(chatId));
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
             if (text.equals("Barcha filiallar")){
                 try {
                     execute(myBotService.filials(chatId));
@@ -71,6 +65,109 @@ public class MyBot extends TelegramLongPollingBot {
                     throw new RuntimeException(e);
                 }
             }
+            if (text.equals("Max Way Aviasozlar")){
+                try {
+                    execute(myBotService.infoAviasozlar(chatId));
+                    execute(myBotService.locationAviasozlar(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (text.equals("Max Way Risoviy")){
+                try {
+                    execute(myBotService.infoRisoviy(chatId));
+                    execute(myBotService.locationRisoviy(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (text.equals("Max Way Parus")){
+                try {
+                    execute(myBotService.infoParus(chatId));
+                    execute(myBotService.locationParus(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (text.equals("Max Way Parkent")){
+                try {
+                    execute(myBotService.infoParkent(chatId));
+                    execute(myBotService.locationParkent(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (text.equals("Max Way Universam")){
+                try {
+                    execute(myBotService.infoUniversam(chatId));
+                    execute(myBotService.locationUniversam(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (text.equals("Max Way Royson")){
+                try {
+                    execute(myBotService.infoRoyson(chatId));
+                    execute(myBotService.locationRoyson(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (text.equals("Max Way Muqimiy")){
+                try {
+                    execute(myBotService.infoMuqimiy(chatId));
+                    execute(myBotService.locationMuqimiy(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (text.equals("🛍" + "Buyurtma berish")){
+                try {
+                    execute(myBotService.orderMenu(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (text.equals("Olib ketish")){
+                try {
+                    execute(myBotService.filialsForOrders(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (text.equals("\uD83D\uDCCDMax Way Beruniy")){
+                try {
+                    execute(myBotService.menuFilial(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (text.equals("Biz haqimizda")){
+                try {
+                    execute(myBotService.aboutUs(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+
+
+            /*if (text.equals("Lavash")){
+                try {
+                    execute(myBotService.sendPhotoLavash(chatId));
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
+            }*/
+
+            /*if (text.equals("Lavash")){
+                EditMessageText editMessageText = new EditMessageText();
+                editMessageText.setChatId(chatId);
+                editMessageText.setMessageId(callbackQuary);
+            }*/
 
 
             /*if (text.equals("/start")){
@@ -96,6 +193,20 @@ public class MyBot extends TelegramLongPollingBot {
             }
 
 
+        }
+        else if(update.hasCallbackQuery()){
+            Long chatId = update.getMessage().getChatId();
+
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String data = callbackQuery.getData();
+            System.out.println(data);
+
+            if (data.equals("Lavash")){
+                EditMessageText editMessageText = new EditMessageText();
+                editMessageText.setChatId(chatId);
+                editMessageText.setMessageId(callbackQuery.getMessage().getMessageId());
+
+            }
         }
     }
 
--- src/main/java/org/example/MyBotService.java
+++ src/main/java/org/example/MyBotService.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
@@ -166,29 +171,29 @@ public class MyBotService {
         row.add(button);
 
         KeyboardButton button1 = new KeyboardButton();
-        button1.setText("Max Way Mega Planet");
+        button1.setText("Max Way Aviasozlar");
         row.add(button1);
         rowList.add(row);
 
         KeyboardRow row1 = new KeyboardRow();
         KeyboardButton button2 = new KeyboardButton();
-        button2.setText("Max Way Aviasozlar");
+        button2.setText("Max Way Risoviy");
         row1.add(button2);
 
         KeyboardButton button3 = new KeyboardButton();
-        button3.setText("Max Way Risoviy");
+        button3.setText("Max Way Parus");
         row1.add(button3);
-        rowList.add(row);
+        rowList.add(row1);
 
         KeyboardRow row2 = new KeyboardRow();
         KeyboardButton button4 = new KeyboardButton();
-        button4.setText("Max Way Parus");
+        button4.setText("Max Way Parkent");
         row2.add(button);
 
         KeyboardButton button5 = new KeyboardButton();
-        button5.setText("Max Way Parkent");
+        button5.setText("Max Way Universam");
         row2.add(button5);
-        rowList.add(row);
+        rowList.add(row2);
 
         KeyboardRow row3 = new KeyboardRow();
         KeyboardButton button6 = new KeyboardButton();
@@ -197,6 +202,62 @@ public class MyBotService {
 
         KeyboardButton button7 = new KeyboardButton();
         button7.setText("Max Way Muqimiy");
+        row3.add(button7);
+        rowList.add(row3);
+
+        //Dalshe qolgan filiallarni qo'shish kerak
+
+
+        replyKeyboardMarkup.setKeyboard(rowList);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        return sendMessage;
+    }
+    public SendMessage filialsForOrders(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("\uD83D\uDCCDFiliallar ro'yxati");
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        List<KeyboardRow> rowList = new ArrayList<>();
+
+        KeyboardRow row = new KeyboardRow();
+        KeyboardButton button = new KeyboardButton();
+        button.setText("\uD83D\uDCCDMax Way Beruniy");
+        row.add(button);
+
+        KeyboardButton button1 = new KeyboardButton();
+        button1.setText("\uD83D\uDCCDMax Way Aviasozlar");
+        row.add(button1);
+        rowList.add(row);
+
+        KeyboardRow row1 = new KeyboardRow();
+        KeyboardButton button2 = new KeyboardButton();
+        button2.setText("\uD83D\uDCCDMax Way Risoviy");
+        row1.add(button2);
+
+        KeyboardButton button3 = new KeyboardButton();
+        button3.setText("\uD83D\uDCCDMax Way Parus");
+        row1.add(button3);
+        rowList.add(row);
+
+        KeyboardRow row2 = new KeyboardRow();
+        KeyboardButton button4 = new KeyboardButton();
+        button4.setText("\uD83D\uDCCDMax Way Parkent");
+        row2.add(button);
+
+        KeyboardButton button5 = new KeyboardButton();
+        button5.setText("\uD83D\uDCCDMax Way Universam");
+        row2.add(button5);
+        rowList.add(row);
+
+        KeyboardRow row3 = new KeyboardRow();
+        KeyboardButton button6 = new KeyboardButton();
+        button6.setText("\uD83D\uDCCDMax Way Royson");
+        row3.add(button6);
+
+        KeyboardButton button7 = new KeyboardButton();
+        button7.setText("\uD83D\uDCCDMax Way Muqimiy");
         row3.add(button7);
         rowList.add(row);
 
@@ -222,6 +283,118 @@ public class MyBotService {
         sendLocation.setChatId(chatId);
         sendLocation.setLatitude(41.344430);
         sendLocation.setLongitude(69.205021);
+        return sendLocation;
+    }
+
+    public SendMessage infoAviasozlar(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Filial: Max Way Aviasozlar" + "\n" + "Manzil:  Aviasozlar ko'chasi, 23, Toshkent" +
+                "\n" + "Orientir: " + "\n" + "Telefon raqami: +998712005400" + "\n" + "Ish vaqti : 10:00 - 03:00");
+        return sendMessage;
+    }
+
+    public SendLocation locationAviasozlar(Long chatId){
+        SendLocation sendLocation = new SendLocation();
+        sendLocation.setChatId(chatId);
+        sendLocation.setLatitude(41.290894);
+        sendLocation.setLongitude(69.342153);
+        return sendLocation;
+    }
+
+    public SendMessage infoRisoviy(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Filial: Max Way Risoviy" + "\n" + "Manzil:  Oltinko'l ko'chasi, 10, Toshkent" +
+                "\n" + "Orientir: Tantana to'yxonasi" + "\n" + "Telefon raqami: +998712005400" + "\n" + "Ish vaqti : 10:00 - 23:00");
+        return sendMessage;
+    }
+
+    public SendLocation locationRisoviy(Long chatId){
+        SendLocation sendLocation = new SendLocation();
+        sendLocation.setChatId(chatId);
+        sendLocation.setLatitude(41.274899);
+        sendLocation.setLongitude(69.309423);
+        return sendLocation;
+    }
+
+    public SendMessage infoParus(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Filial: Max Way Parus" + "\n" + "Manzil:  Qatortol ko'chasi, 60/5, Toshkent" +
+                "\n" + "Orientir: Parus savdo markazi" + "\n" + "Telefon raqami: +998712005400" + "\n" + "Ish vaqti : 10:00 - 03:00");
+        return sendMessage;
+    }
+
+    public SendLocation locationParus(Long chatId){
+        SendLocation sendLocation = new SendLocation();
+        sendLocation.setChatId(chatId);
+        sendLocation.setLatitude(41.293536);
+        sendLocation.setLongitude(69.212856);
+        return sendLocation;
+    }
+
+    public SendMessage infoParkent(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Filial: Max Way Parkent" + "\n" + "Manzil:  Parkent ko'chasi, 30B, Toshkent" +
+                "\n" + "Orientir: Parkent bozori" + "\n" + "Telefon raqami: +998712005400" + "\n" + "Ish vaqti : 10:00 - 03:00");
+        return sendMessage;
+    }
+
+    public SendLocation locationParkent(Long chatId){
+        SendLocation sendLocation = new SendLocation();
+        sendLocation.setChatId(chatId);
+        sendLocation.setLatitude(41.314772);
+        sendLocation.setLongitude(69.325067);
+        return sendLocation;
+    }
+
+    public SendMessage infoUniversam(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Filial: Max Way Universam" + "\n" + "Manzil:  Amir Temur shox ko'chasi, 41/3, Toshkent" +
+                "\n" + "Orientir: Universam Yunusobod" + "\n" + "Telefon raqami: +998712005400" + "\n" + "Ish vaqti : 10:00 - 23:00");
+        return sendMessage;
+    }
+
+    public SendLocation locationUniversam(Long chatId){
+        SendLocation sendLocation = new SendLocation();
+        sendLocation.setChatId(chatId);
+        sendLocation.setLatitude(41.363468);
+        sendLocation.setLongitude(69.288586);
+        return sendLocation;
+    }
+
+    public SendMessage infoRoyson(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Filial: Max Way Royson" + "\n" + "Manzil:  Zarqaynar ko'chasi, 2, Toshkent" +
+                "\n" + "Orientir: Sirk" + "\n" + "Telefon raqami: +998712005400" + "\n" + "Ish vaqti : 10:00 - 01:00");
+        return sendMessage;
+    }
+
+    public SendLocation locationRoyson(Long chatId){
+        SendLocation sendLocation = new SendLocation();
+        sendLocation.setChatId(chatId);
+        sendLocation.setLatitude(41.322643);
+        sendLocation.setLongitude(69.241973);
+        return sendLocation;
+    }
+
+    public SendMessage infoMuqimiy(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Filial: Max Way Muqimiy" + "\n" + "Manzil:  Chilonzor ko'chasi, Toshkent" +
+                "\n" + "Orientir: Chilonzor 1-kvartal" + "\n" + "Telefon raqami: +998712005400" + "\n" + "Ish vaqti : 10:00 - 03:00");
+        return sendMessage;
+    }
+
+    public SendLocation locationMuqimiy(Long chatId){
+        SendLocation sendLocation = new SendLocation();
+        sendLocation.setChatId(chatId);
+        sendLocation.setLatitude(41.287875);
+        sendLocation.setLongitude(69.229238);
         return sendLocation;
     }
 
@@ -278,9 +451,94 @@ public class MyBotService {
         button8.setText("Shaurma");
         row4.add(button8);
         rowList.add(row4);
-        return sendMessage;
+
         //Qolgan menyuni kiritib chiqish
+        replyKeyboardMarkup.setKeyboard(rowList);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        return sendMessage;
+
+
+
     }
+
+    public SendMessage sendMessageInline(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText("Quyidagilardan birini tanlang");
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<InlineKeyboardButton> row= new ArrayList<>();
+        List<List<InlineKeyboardButton>> rowlist = new ArrayList<>();
+
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText("Mini");
+        button.setCallbackData("miniId");
+        row.add(button);
+        rowlist.add(row);
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("Standart");
+        button1.setCallbackData("standartId");
+        row.add(button1);
+
+        InlineKeyboardButton button2 = new InlineKeyboardButton();
+        button2.setText("Big");
+        button2.setCallbackData("bigId");
+        row.add(button2);
+
+        inlineKeyboardMarkup.setKeyboard(rowlist);
+        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+        return sendMessage;
+    }
+
+    public SendPhoto sendPhotoLavash(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://maxway.uz/_next/image?url=https%3A%2F%2Fcdn.delever.uz%2Fdelever%2F750f3234-3fa8-4aab-b763-0e37dd82f609&w=1920&q=75"));
+        sendPhoto.setCaption("Ixtiyoriy birini tanlang");
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        List<List<InlineKeyboardButton>> rowlist = new ArrayList<>();
+
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText("Big");
+        button.setCallbackData("bigId");
+        row.add(button);
+        rowlist.add(row);
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("Standart");
+        button1.setCallbackData("standartId");
+        row.add(button1);
+
+        InlineKeyboardButton button2 = new InlineKeyboardButton();
+        button2.setText("Mini");
+        button2.setCallbackData("miniId");
+        row.add(button2);
+
+        inlineKeyboardMarkup.setKeyboard(rowlist);
+        sendPhoto.setReplyMarkup(inlineKeyboardMarkup);
+        return sendPhoto;
+    }
+
+    public SendPhoto aboutUs(Long chatId){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(chatId);
+        sendPhoto.setPhoto(new InputFile("https://maxway.uz/_next/image?url=%2Fimages%2Fabout.png&w=1920&q=75"));
+        sendPhoto.setCaption("""
+                <b>Max Way</b>
+                <b>Aloqa markazi</b> "+998712005400"
+                <a href = "https://maxway.uz/about"> Biz haqimizda</a>
+                """);
+        sendPhoto.setParseMode(ParseMode.HTML);
+        return sendPhoto;
+    }
+
+
+
 
 
 
